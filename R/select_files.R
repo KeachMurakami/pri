8dd4d4ae7a887c7d5b34b9dffc782803823cc923
MurakamiKeach <m.keach.11@gmail.com>
2017-06-22 10:31:37 +0900

    fix bare package::function (ungroup)

--- R/select_files.R
+++ R/select_files.R
@@ -1,22 +1,22 @@
 sample_files_by_time <-
   function(files, times = cumsum(1:100), init = "2017-01-01 00:00:00"){
-  
-    file_mtime <- 
+
+    file_mtime <-
       files %>%
       file.mtime %>%
       as.numeric
-  
+
     init_time <-
       init %>%
       as.POSIXct %>%
       as.numeric
-  
+
     sample_time <-
       init_time + times
-  
+
     ip <- (sample_time + lag(sample_time))/2
     cuts <- c(-Inf, ip[-1], Inf)
-  
+
     tibble::tibble(path = files, mtime = file_mtime) %>%
       dplyr::mutate(target_time = sample_time[findInterval(mtime, cuts)],
                     delta = file_mtime - target_time,
@@ -24,7 +24,7 @@ sample_files_by_time <-
       dplyr::group_by(target_time) %>%
       dplyr::arrange(abs(delta)) %>%
       dplyr::slice(1) %>%
-      dplyr::ungroup %>%
+      dplyr::ungroup() %>%
       dplyr::mutate(mtime = as.POSIXct(mtime, origin = "1970-01-01"),
                     target_time = as.POSIXct(target_time, origin = "1970-01-01"),
                     init_time = init)
@@ -36,7 +36,7 @@ rough_join <-
     df_y <- sample_files_by_time(files_y, ...)
     dplyr::left_join(df_x, df_y, by = c("target_time", "target_time_sec", "init_time")) %>%
       dplyr::filter(abs(delta.x) <= acceptable_delta, abs(delta.y) <= acceptable_delta)
-      
+
   }
 
 ## demo code for me
